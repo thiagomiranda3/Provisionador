436437ac26a771a1dfd68681b3d788c9d4366667
Thiago Miranda <marine.rlivielor@gmail.com>
2014-10-17 17:35:08 -0300

    Equações lidas por arquivo

--- lib/controllers/provision_control.rb
+++ lib/controllers/provision_control.rb
@@ -10,11 +10,24 @@ module Escalonador
       @provision.set_conn(obj_conn)
     end
 
+    def read_file()
+      arquivo = Array.new
+      file = File.expand_path("../lib/equacoes.txt")
+      File.open(file, "r") do |file|
+        cpus = file.readline()
+        file.each_line { |line|
+          arquivo << line.split.map(&:to_f)
+        }
+      end
+      return arquivo
+    end
+
     def provisionar
       acessos = num_acessos
-      num_memory = define_memory(acessos.to_i)
-      num_vcpu = define_vcpu(acessos.to_i)
-      if num_memory == -1 || num_vcpu == -1
+      recursos = define_resources(acessos.to_i)
+      num_memory = recursos[0]
+      num_vcpu = recursos[1]
+      if recursos[0] == -1 || recursos[1] == -1
         puts "Servidor não suporta esse número de acessos"
       else
         @provision.provisionar(num_memory, num_vcpu)
@@ -27,36 +40,31 @@ module Escalonador
       @main_view.menu_acessos
     end
 
-    def define_memory(num_acessos)
-      if num_acessos < 9
-        return 512
-      elsif num_acessos <= 17 && num_acessos >= 9
-        return 27.6669*Math::E**(0.32702*num_acessos)
-      elsif num_acessos <= 36 && num_acessos >= 18
-        return 39.9929*Math::E**(0.142771*num_acessos)
-      elsif num_acessos <= 53 && num_acessos >= 37
-        return 1.39896*Math::E**(0.159945*num_acessos)
-      elsif num_acessos <= 70 && num_acessos >= 54
-        return 0.104909*Math::E**(0.157865*num_acessos)
-      else
-        return -1
-      end
-    end
+    def define_resources(num_acessos)
+      recursos = Array.new(2, -1)
+      valores = read_file
 
-    def define_vcpu(num_acessos)
-      if num_acessos < 9
-        return 1
-      elsif num_acessos <= 17 && num_acessos >= 9
-        return 1
-      elsif num_acessos <= 36 && num_acessos >= 18
-        return 2
-      elsif num_acessos <= 53 && num_acessos >= 37
-        return 3
-      elsif num_acessos <= 70 && num_acessos >= 54
-        return 4
-      else
-        return -1
+      vcpu = 0
+      valores.each do |valor|
+        vcpu += 1
+        val_1 = valor[0]
+        val_2 = valor[1]
+        coef_max = valor[2]
+        coef_min = valor[3]
+        if num_acessos <= coef_max && num_acessos >= coef_min
+          memoria=(val_1*Math::E**(val_2*num_acessos))
+          recursos[0] = memoria
+          recursos[1] = vcpu
+          return recursos
+        else
+          if num_acessos < coef_min
+            recursos[0]=512
+            recursos[1]=vcpu
+            return recursos
+          end
+        end
       end
+      return recursos
     end
 
   end
